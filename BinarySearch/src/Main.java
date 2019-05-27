1825aedabd6da90df43c8f41fe84e9261442e081
Victor A <viktoray007@gmail.com>
2019-05-27 18:04:23 +0300

    add recursive samples

--- BinarySearch/src/Main.java
+++ BinarySearch/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
 
@@ -24,6 +25,12 @@ public class Main {
 
         boolean pal = isPalindrome("madam");
         System.out.println(pal);
+
+        IntStream.range(0, 10).forEach(n -> System.out.println(fun(n, n+1)));
+        System.out.println(fun2(100, 80));
+        System.out.println(rec(5));
+        System.out.println(rec2(1000));
+
     }
 
     public static int sequencialSearch(int [] array, int key){
@@ -71,4 +78,39 @@ public class Main {
         if (s.charAt(0) != s.charAt(s.length()-1)) return false;
         return isPalindrome(s.substring(1, s.length() - 1));
     }
+
+    static int fun(int x, int y){
+        System.out.println(x + " " + y);
+        if (x == 0) return y;
+        return fun(x - 1, x + y);
+    }
+
+    static int fun2(int x, int y){
+        System.out.println(x + " " + y);
+        return (y == 0)? 0: (y % 2 == 0)?fun2(x + x, y/2):fun2(x+x, y/2) + x;
+    }
+
+    static int rec(int a){
+        return (a == 40)? a : a + rec(a * 2);
+    }
+
+    static int rec2(int n){
+        return (n > 100)? n - 10: rec2(rec2(n + 11));
+    }
 }
+
+//a = 4 b = 3
+//a = 8 b = 1 + 4
+//a = 16 b = 0 + 8
+//0        + 8        + 4
+//12
+//
+//a = 5
+//5 + 10 + 20 + 40
+
+//n = 99
+//n = 110
+//n = 100
+//n = 111
+// n = 101
+// n = 91
