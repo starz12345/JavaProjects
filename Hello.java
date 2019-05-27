8f682c354ba7b0c87198624f3996339ab556d41d
Victor A <viktoray007@gmail.com>
2019-05-27 14:18:56 +0300

    add Binary Search

--- Hello.java
+++
@@ -1,9 +0,0 @@
-public class Hello {
-    public static void main ( String [] argds) {
-        //Prints "Hello, World!" to the terminal window
-        System.out.println("Hello, World! Life is Beautiful");
-        System.out.println("Hello, World! Life is Beautiful");
-        System.out.println("Hello, World! Life is Beautiful");
-        System.out.println("Hello, World! Life is Beautiful");
-    }//end of main
-}//end of program
